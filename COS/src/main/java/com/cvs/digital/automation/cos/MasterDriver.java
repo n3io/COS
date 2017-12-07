c4c85d910cb6acafd8a1df6098385eb94c621d1d
kalidass <kalidass@docker-host>
2017-12-07 21:26:22 +0000

    Add simple out change

--- COS/src/main/java/com/cvs/digital/automation/cos/MasterDriver.java
+++ COS/src/main/java/com/cvs/digital/automation/cos/MasterDriver.java
@@ -5,6 +5,7 @@ public class MasterDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git");
+		System.out.println("Git-leve2");
 		
 	}
 
